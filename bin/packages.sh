b063c527ec91cd5fe6efee87f4530148e345ea49
Technote <technote.space@gmail.com>
2019-07-17 02:32:16 +0900

    feat: ci

---
+++ bin/packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+packages+=( "wp-content-framework/core" )
+packages+=( "wp-content-framework/update" )
+packages+=( "wp-content-framework/update_check" )
+packages+=( "wp-content-framework/editor" )
