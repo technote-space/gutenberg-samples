4733508c6b313edf699d33beefffcbfea9810750
Technote <technote.space@gmail.com>
2019-10-20 15:03:03 +0900

    v0.1.5

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: Gutenberg samples
  * Author: Technote
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author URI: https://technote.space
  * Text Domain: gutenberg-samples
  * Domain Path: /languages/
