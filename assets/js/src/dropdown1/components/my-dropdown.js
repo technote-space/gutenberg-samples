aa4fceb40b73de2744388d35d578959800b26e21
Technote <technote.space@gmail.com>
2020-06-21 19:18:33 +0900

    chore: code formattings

--- assets/js/src/dropdown1/components/my-dropdown.js
+++ assets/js/src/dropdown1/components/my-dropdown.js
@@ -1,22 +1,22 @@
 /* istanbul ignore next */
-const { BlockFormatControls } = wp.blockEditor && wp.blockEditor.BlockEdit ? wp.blockEditor : wp.editor;
+const { BlockFormatControls }   = wp.blockEditor && wp.blockEditor.BlockEdit ? wp.blockEditor : wp.editor;
 const { Toolbar, DropdownMenu } = wp.components;
 import MyDropdownControls from './my-dropdown-controls';
 
 const MyDropdown = () => <BlockFormatControls>
-	<div className="editor-format-toolbar block-editor-format-toolbar">
-		<Toolbar>
-			<MyDropdownControls.Slot>
-				{ fills => <DropdownMenu
-					className='components-dropdown-button'
-					icon='admin-customizer'
-					popoverProps={ ( { position: 'bottom left' } ) }
-					label='dropdown'
-					controls={ fills.map( ( [ { props } ] ) => props ) }
-				/> }
-			</MyDropdownControls.Slot>
-		</Toolbar>
-	</div>
+  <div className="editor-format-toolbar block-editor-format-toolbar">
+    <Toolbar>
+      <MyDropdownControls.Slot>
+        {fills => <DropdownMenu
+          className='components-dropdown-button'
+          icon='admin-customizer'
+          popoverProps={({ position: 'bottom left' })}
+          label='dropdown'
+          controls={fills.map(([{ props }]) => props)}
+        />}
+      </MyDropdownControls.Slot>
+    </Toolbar>
+  </div>
 </BlockFormatControls>;
 
 export default MyDropdown;
--- assets/js/src/dropdown1/index.js
+++ assets/js/src/dropdown1/index.js
@@ -3,16 +3,16 @@ const { registerFormatType } = wp.richText;
 import { createToolbarButton, getRichTextSetting } from './utils';
 
 [
-	{
-		name: 'test1',
-		create: createToolbarButton,
-	},
-	{
-		name: 'test2',
-		create: createToolbarButton,
-	},
-	{
-		name: 'test3',
-		create: createToolbarButton,
-	},
-].forEach( ( { name, create, setting = {} }, index ) => registerFormatType( ...getRichTextSetting( { name, create, setting }, index ) ) );
+  {
+    name: 'test1',
+    create: createToolbarButton,
+  },
+  {
+    name: 'test2',
+    create: createToolbarButton,
+  },
+  {
+    name: 'test3',
+    create: createToolbarButton,
+  },
+].forEach(({ name, create, setting = {} }, index) => registerFormatType(...getRichTextSetting({ name, create, setting }, index)));
