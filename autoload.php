5488a469d3b381c7fc265d9742b673db205d2071
Technote <technote.space@gmail.com>
2021-02-28 02:30:58 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: Gutenberg samples
  * Author: Technote
- * Version: 1.1.5
+ * Version: 1.1.6
  * Author URI: https://technote.space
  * Text Domain: gutenberg-samples
  * Domain Path: /languages/
